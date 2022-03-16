512eb8a6b2284487a486515f637054470b427752
niklasjohto <niklasjohto@tutanota.com>
2022-03-16 18:32:47 +0100

    :zap::fire: added unifiOS support & removed cookie parser function (not needed)

--- src/unifi.ts
+++ src/unifi.ts
@@ -1,6 +1,5 @@
 import fetch, { BodyInit, Response } from "node-fetch";
 import https from "https";
-import { parseCookies } from "./cookies.js";
 
 interface UnifiOptions {
   host?: string;
@@ -9,6 +8,7 @@ interface UnifiOptions {
   password?: string;
   site?: string;
   sslStrict?: boolean;
+  unifiOS?: boolean;
 }
 
 interface FetchOptions {
@@ -21,12 +21,14 @@ type FetchReturn = Promise<Response | void>;
 class Unifi {
   // Unifi Controller options
   private host: string;
+  private hostHREF: string;
   private hostAPI: string;
   private port: number;
   private username: string;
   private password: string;
   private site: string;
   private sslStrict: boolean;
+  private unifiOS: boolean;
 
   // Session variables
   private cookies: string;
@@ -37,7 +39,13 @@ class Unifi {
   constructor(options?: UnifiOptions) {
     this.port = options?.port ?? 8443;
     this.host = options?.host ?? "unifi";
-    this.hostAPI = `https://${this.host}:${this.port}/api`;
+    this.unifiOS = options?.unifiOS ?? false;
+
+    this.hostHREF = `https://${this.host}:${this.port}`;
+    this.hostAPI = `${this.hostHREF}/api`;
+    if (this.unifiOS) {
+      this.hostAPI = `${this.hostHREF}/proxy/network/api`;
+    }
 
     this.username = options?.username ?? "admin";
     this.password = options?.password ?? "ubnt";
@@ -55,13 +63,15 @@ class Unifi {
     const login = await this._login();
 
     if (!login.ok) {
-      return console.log(`${date}: Could not connect to the UniFi Controller`);
+      return console.log(
+        `${date}: Could not connect to the UniFi Controller\nReason: error ${login.status}`
+      );
     }
 
     this.isLoggedIn = true;
 
     // Store the session cookies
-    this.cookies = parseCookies(login);
+    this.cookies = login.headers.get("set-cookie") as string;
 
     // This is mostly for debugging...
     console.log(`${date}: User ${this.username} logged in`);
@@ -74,7 +84,17 @@ class Unifi {
       return console.log(`${date}: You are not logged in`);
     }
 
-    await this._logout();
+    const response = await this._logout();
+
+    if (!response?.ok) {
+      return console.log(
+        `${date}: There was an error logging out\nReason: error ${
+          response!.status
+        }`
+      );
+    }
+
+    this.isLoggedIn = false;
 
     // Delete the session cookies
     this.cookies = "";
@@ -84,49 +104,53 @@ class Unifi {
   }
 
   private async _login(): Promise<Response> {
-    return await fetch(`${this.hostAPI}/login`, {
+    let endpointURL = `${this.hostHREF}/api/login`;
+    if (this.unifiOS) {
+      endpointURL = `${this.hostHREF}/api/auth/login`;
+    }
+
+    return await fetch(endpointURL, {
       method: "POST",
       agent: this.agent,
       body: JSON.stringify({
         // Send the account credentials with the request
         username: this.username,
         password: this.password,
-        remember: true,
+        remember: false,
+        strict: true,
       }),
     });
   }
 
   private async _logout(): Promise<Response | void> {
-    return await fetch(`${this.hostAPI}/logout`, {
-      method: "GET",
+    return await fetch(`${this.hostHREF}/api/logout`, {
+      method: "POST",
       agent: this.agent,
+      headers: {
+        cookie: this.cookies,
+      },
     });
   }
 
-  /**
-   * @description Fetch method with a default configuration
-   *
-   * @param endpoint
-   * @param options
-   * @returns Promise<Response | void>
-   */
-  public async call(endpoint: string, options?: FetchOptions): FetchReturn {
+  private async _call(
+    endpointURL: string,
+    options?: FetchOptions
+  ): FetchReturn {
     const date = new Date().toLocaleString("en-DK");
 
     if (!this.isLoggedIn) {
       return console.log(`${date}: You are not logged in`);
     }
 
-    const endpointURL = `${this.hostAPI}/s/${this.site}/${endpoint}`;
-
     // Check if user has provided a method or if there's a payload,
     // POST must be used if there's a payload
     const method = options?.method ?? options?.payload ? "POST" : "GET";
 
     // Check if user has provided a payload
-    const body: BodyInit | undefined = options?.payload
-      ? JSON.stringify({ cmd: options.payload })
-      : undefined;
+    let body: BodyInit | undefined = undefined;
+    if (options?.payload) {
+      body = JSON.stringify({ cmd: options.payload });
+    }
 
     const response = await fetch(endpointURL, {
       agent: this.agent,
@@ -139,13 +163,23 @@ class Unifi {
 
     if (!response.ok) {
       return console.log(
-        `${date}: Invalid configuration, could not reach ${endpointURL}`
+        `${date}: Could not reach ${endpointURL}\nReason: error ${response.status}`
       );
     }
 
     const data = await response.json();
 
     return data;
+  }
+
+  public ctrlCall(endpoint: string, options?: FetchOptions) {
+    const endpointURL = `${this.hostAPI}/${endpoint}`;
+    return this._call(endpointURL, options);
+  }
+
+  public siteCall(endpoint: string, options?: FetchOptions) {
+    const endpointURL = `${this.hostAPI}/s/${this.site}/${endpoint}`;
+    return this._call(endpointURL, options);
   }
 }
 
